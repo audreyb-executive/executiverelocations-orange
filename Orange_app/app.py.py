854010debcfe2632e8b61c83ed938498aa310b77
audreyb-executive <audrey.b.executive@gmail.com>
2025-11-18 11:40:08 +0100

    Add files via upload

---
+++ Orange_app/app.py.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Configuration de la page (titre dans l’onglet, largeur, etc.)
+st.set_page_config(
+    page_title="Boîte à outils Executive Relocations",
+    layout="wide"
+)
+
+# Titre principal
+st.title("Bienvenue sur la boîte à outils d'Executive Relocations")
+
+# Message d’explication
+st.info("← Sélectionnez une application dans le menu de gauche pour commencer.")
